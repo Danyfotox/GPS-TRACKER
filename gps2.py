7cbe8b71fc78935805ae4ee8a9f2e4bb4d3a3d93
Danyfotox <79683780+Danyfotox@users.noreply.github.com>
2021-06-16 02:23:47 -0500

    Add files via upload

---
+++ gps2.py
@@ -0,0 +1,38 @@
+import serial
+import time
+import string
+import sys
+import pynmea2
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+from pubnub.exceptions import PubNubException
+
+pnChannel = "raspi-tracker";
+
+pnconfig = PNConfiguration()
+pnconfig.subscribe_key = "sub-c-8d914a6a-c41b-11eb-9e40-ea6857a81ff7"
+pnconfig.publish_key = "pub-c-9b4849dd-1ce9-4e21-800e-59be2d2b8507"
+pnconfig.ssl = False
+ 
+pubnub = PubNub(pnconfig)
+pubnub.subscribe().channels(pnChannel).execute()
+
+while True:
+    port="/dev/ttyS0"
+    ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+    dataout = pynmea2.NMEAStreamReader()
+    newdata=ser.readline()
+    if sys.version_info[0] == 3: 
+              newdata = newdata.decode("utf-8","ignore")
+    if newdata[0:6] == "$GPRMC":
+        newmsg=pynmea2.parse(newdata)
+        lat=newmsg.latitude
+        lng=newmsg.longitude
+        try:
+            envelope = pubnub.publish().channel(pnChannel).message({
+            'lat':lat,
+            'lng':lng
+            }).sync()
+            print("publish timetoken: %d" % envelope.result.timetoken)
+        except PubNubException as e:
+            handle_exception(e)
